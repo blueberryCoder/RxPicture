c2acd9a038b6d0ab944ffd0ce8ebdc0c2185bce8
unknown <>
2017-11-15 10:44:51 +0800

    tijiao

---
+++ picture/src/main/java/com/blueberry/picture/RxPicture.java
@@ -0,0 +1,157 @@
+/*
+ *    Copyright 2017 blueberry
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.blueberry.picture;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.functions.Function;
+import io.reactivex.subjects.PublishSubject;
+
+
+/**
+ * Created by blueberry on 11/14/2017.
+ */
+
+public class RxPicture {
+    private static final String TAG = "RxPicture";
+
+    static final String FRAGMENT_TAG = "RxPictureFragmentTag";
+    static final Object TRIGGER = new Object();
+
+    public String fileProvideAuthority = "com.blueberry.picture.fileprovider";
+
+    private RxPictureFragment mRxPictureFragment;
+
+    public RxPicture(AppCompatActivity appCompatActivity) {
+        this.mRxPictureFragment = getRxPictureFragment(appCompatActivity);
+    }
+
+    private RxPictureFragment getRxPictureFragment(AppCompatActivity appCompatActivity) {
+        RxPictureFragment rxPictureFragment = findRxPictureFragment(appCompatActivity);
+        if (null == rxPictureFragment) {
+            rxPictureFragment = RxPictureFragment.newInstance();
+            FragmentManager supportFragmentManager = appCompatActivity.getSupportFragmentManager();
+            supportFragmentManager.beginTransaction()
+                    .add(rxPictureFragment, FRAGMENT_TAG)
+                    .commitAllowingStateLoss();
+            supportFragmentManager.executePendingTransactions();
+        }
+        return rxPictureFragment;
+    }
+
+    private RxPictureFragment findRxPictureFragment(AppCompatActivity appCompatActivity) {
+        return (RxPictureFragment) appCompatActivity.getSupportFragmentManager()
+                .findFragmentByTag(FRAGMENT_TAG);
+    }
+
+    /**
+     * 设置FileProviderAuthority.
+     *
+     * @param fileProvideAuthority
+     */
+    public void setFileProvideAuthority(String fileProvideAuthority) {
+        this.fileProvideAuthority = fileProvideAuthority;
+    }
+
+
+    /**
+     * 请求拍照
+     *
+     * @return
+     */
+    public Observable<String> requestTakePicture() {
+        return Observable.just(TRIGGER).compose(ensureTakePicture());
+    }
+
+    /**
+     * 请求选择图片
+     *
+     * @return
+     */
+    public Observable<String> requestSelectFromAlbum() {
+        return Observable.just(TRIGGER).compose(ensureSelectFromAlbum());
+    }
+
+    /**
+     * 这个可以用来组合别的框架，例如RxView
+     *
+     * @param <T>
+     * @return
+     */
+    public <T> ObservableTransformer<T, String> ensureSelectFromAlbum() {
+        return new ObservableTransformer<T, String>() {
+            @Override
+            public ObservableSource<String> apply(Observable<T> upstream) {
+                return upstream.flatMap(new Function<T, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(T t) throws Exception {
+                        return selectFromAlbum();
+                    }
+                });
+            }
+        };
+    }
+
+    /**
+     * 这个可以用来组合别的框架。例如RxView
+     *
+     * @param <T>
+     * @return
+     */
+    public <T> ObservableTransformer<T, String> ensureTakePicture() {
+        return new ObservableTransformer<T, String>() {
+            @Override
+            public ObservableSource<String> apply(Observable<T> upstream) {
+                return upstream.flatMap(new Function<T, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(T t) throws Exception {
+                        return takePicture();
+                    }
+                });
+            }
+        };
+    }
+
+
+    private Observable<String> takePicture() {
+        PublishSubject<String> subject
+                = mRxPictureFragment.getSubjectByKey(RxPictureFragment.TAKE_PICTURE);
+        if (subject == null) {
+            subject = PublishSubject.create();
+            mRxPictureFragment.setSubject(RxPictureFragment.TAKE_PICTURE, subject);
+        }
+
+        mRxPictureFragment.takePicture(fileProvideAuthority);
+        return subject;
+    }
+
+
+    private Observable<String> selectFromAlbum() {
+        PublishSubject<String> subject = mRxPictureFragment
+                .getSubjectByKey(RxPictureFragment.SELECT_FROM_ALBUM);
+        if (subject == null) {
+            subject = PublishSubject.create();
+            mRxPictureFragment.setSubject(RxPictureFragment.SELECT_FROM_ALBUM, subject);
+        }
+        mRxPictureFragment.selectPhotoFromAlbum();
+        return subject;
+    }
+}
---
+++ picture/src/main/java/com/blueberry/picture/RxPictureFragment.java
@@ -0,0 +1,178 @@
+/*
+ *    Copyright 2017 blueberry
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.blueberry.picture;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+import java.util.HashMap;
+
+import io.reactivex.subjects.PublishSubject;
+
+/**
+ * Created by blueberry on 11/14/2017.
+ */
+
+public class RxPictureFragment extends Fragment {
+
+    public static final String TAKE_PICTURE = "take_picture";
+    public static final String SELECT_FROM_ALBUM = "select_from_album";
+
+    public static final int TAKE_PICTURE_CODE = 66;
+    public static final int SELECT_PICTURE_CODE = 67;
+
+    private HashMap<String, PublishSubject<String>> mSubjects = new HashMap<>();
+
+    private String tempTakePicturePath;
+
+    public static RxPictureFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        RxPictureFragment fragment = new RxPictureFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+
+    /**
+     * 拍照。
+     *
+     * @return 文件路径。
+     */
+    public String takePicture(String authority) {
+        File file = new File(getActivity().getFilesDir(), "/temp/"
+                + System.currentTimeMillis() + ".jpg");
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        Uri imageUri = FileProvider.getUriForFile(getActivity(), authority, file);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PICTURE_CODE);
+        tempTakePicturePath = file.getAbsolutePath();
+        return file.getAbsolutePath();
+    }
+
+
+    /**
+     * 从相册选择
+     */
+    public void selectPhotoFromAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, SELECT_PICTURE_CODE);
+    }
+
+    public void getImagePathFromAlbumData(Intent data, PublishSubject<String> subject) {
+        Uri uri = data.getData();
+        if (uri == null) {
+            subject.onError(new RuntimeException("获得的data为null"));
+            return;
+        }
+        if (uri.getScheme().equals("file")) {
+            String path = uri.getPath();
+            // output path
+            subject.onNext(path);
+            subject.onComplete();
+        } else {
+            ContentResolver resolver = getActivity().getContentResolver();
+            Cursor cursor = null;
+            try {
+                cursor = resolver.query(uri,
+                        new String[]{MediaStore.Images.Media.DATA}, null,
+                        null, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    String path = cursor.getString(0);
+                    //output path
+                    subject.onNext(path);
+                    subject.onComplete();
+                }
+            } finally {
+                if (null != cursor) {
+                    cursor.close();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_PICTURE_CODE) {
+            handleTakePictureFinished(resultCode);
+        } else if (requestCode == SELECT_PICTURE_CODE) {
+            handleImageFromAlbum(resultCode, data);
+        }
+    }
+
+    private void handleImageFromAlbum(int resultCode, Intent data) {
+        PublishSubject<String> subject = getPublishSubjectFromKey(SELECT_FROM_ALBUM);
+        if (subject == null) return;
+        if (resultCode == Activity.RESULT_OK) {
+            getImagePathFromAlbumData(data, subject);
+        } else {
+            subject.onError(new RuntimeException("拍照失败了"));
+        }
+    }
+
+    private void handleTakePictureFinished(int resultCode) {
+        PublishSubject<String> subject = getPublishSubjectFromKey(TAKE_PICTURE);
+        if (subject == null) return;
+        if (resultCode == Activity.RESULT_OK) {
+            subject.onNext(tempTakePicturePath);
+            subject.onComplete();
+        } else {
+            subject.onError(new RuntimeException("拍照失败了"));
+        }
+    }
+
+    public PublishSubject<String> getSubjectByKey(String key) {
+        return mSubjects.get(key);
+    }
+
+    public PublishSubject<String> setSubject(String key, PublishSubject<String> subject) {
+        return mSubjects.put(key, subject);
+    }
+
+    @Nullable
+    private PublishSubject<String> getPublishSubjectFromKey(String selectFromAlbum) {
+        PublishSubject<String> subject = mSubjects.get(selectFromAlbum);
+        if (subject != null) {
+            mSubjects.remove(selectFromAlbum);
+        }
+        return subject;
+    }
+}
